186f07d5f0ef8381ee8a610c8c85ffea6dca1fd8
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-07-13 13:31:34 +0500

    task 1 task2

---
+++ StaticClasses/6 Task1/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Task1
+{
+    public static class FindAndReplaceManager
+    {
+      
+
+        public static void FindNext(string str)
+        {
+            if (Book.Notes.text.Contains(str))
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+    public class Book
+    {
+        public static class Notes
+        {
+            public static string text = "";
+            public static string Text 
+            { 
+                get 
+                { 
+                    return text;
+                } 
+                set 
+                {
+                    if (text != "")
+                        text += "\n";
+                    text += value;
+                } 
+            }
+        }
+    }
+}
---
+++ StaticClasses/6 Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+  
+            Book.Notes.Text = "Hello";
+            Book.Notes.Text = "World";
+            Console.WriteLine(Book.Notes.text);
+            Console.WriteLine(new string('-', 40));
+            FindAndReplaceManager.FindNext("Hello");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Расширьте пример урока 005_Delegation, создав в классе Book, вложенный класс Notes, который
+//позволит сохранять заметки читателя
