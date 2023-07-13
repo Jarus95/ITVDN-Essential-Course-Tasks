18c56ef78364156b298797c56a14486214ffbea8
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-07-13 12:47:15 +0500

    Extra Task

---
+++ StaticClasses/6 Extra Task/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Extra_Task
+{
+    public static class Calculator
+    {
+        /// <summary>
+        ///  a+b
+        /// </summary>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        ///  a/b
+        /// </summary>
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+        /// <summary>
+        ///  a*b
+        /// </summary>
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        ///  a-b
+        /// </summary>
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ StaticClasses/6 Extra Task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Extra_Task
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(4, 3));
+            Console.WriteLine(Calculator.Multiply(4, 3));
+            Console.WriteLine(Calculator.Subtract(4, 3));
+            Console.WriteLine(Calculator.Divide(4, 3));
+            Console.ReadKey();
+        }
+    }
+}
+
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Требуется:
+//Создать статический класс Calculator, с методами для выполнения основных арифметических
+//операций.
+//Написать программу, которая выводит на экран основные арифметические операции
